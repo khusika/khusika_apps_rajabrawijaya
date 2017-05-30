4994d5c26589e18aaa69f4ca26b6f96529838474
khusika <khusikadhamar@gmail.com>
2017-05-30 11:00:42 +0700

    Main: Add AndroidImageSlider

--- rajabrawijaya/app/src/main/java/com/khusika/rajabrawijaya/fragments/MainFragment.java
+++ rajabrawijaya/app/src/main/java/com/khusika/rajabrawijaya/fragments/MainFragment.java
@@ -4,21 +4,30 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.khusika.rajabrawijaya.MainActivity;
 import com.khusika.rajabrawijaya.R;
+
+import java.util.HashMap;
 
 /**
  * Created by khusika on 29/05/17.
  */
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements ViewPagerEx.OnPageChangeListener {
 
     private MainActivity main;
     private Toolbar toolbar;
+    private SliderLayout mDemoSlider;
 
     public MainFragment() {
         // Required empty public constructor
@@ -34,8 +43,35 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         toolbar = (Toolbar)view.findViewById(R.id.fragment_main_toolbar);
-
         setupToolbar();
+        mDemoSlider = (SliderLayout)view.findViewById(R.id.slider);
+
+        HashMap<String,Integer> file_maps = new HashMap<>();
+        file_maps.put("1",R.color.colorPrimary);
+        file_maps.put("2",R.color.colorAccent);
+        file_maps.put("3",R.color.colorPrimaryDark);
+
+        for(String name : file_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(getActivity());
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            mDemoSlider.addSlider(textSliderView);
+        }
+
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.ZoomOutSlide);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+        mDemoSlider.addOnPageChangeListener(this);
 
         return view;
     }
@@ -51,4 +87,21 @@ public class MainFragment extends Fragment {
         main.setSupportActionBar(toolbar);
     }
 
+    @Override
+    public void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+    @Override
+    public void onPageSelected(int position) {
+        Log.d("Slider Demo", "Page Changed: " + position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {}
 }
